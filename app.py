8f9eedd84a66b22a1aaca02d6a886d5e3c041b0a
necroX0 <anonymous77823@gmail.com>
2024-09-19 08:54:06 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import os, ssl, smtplib
+from email.message import EmailMessage
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+email_sender = "snakekillah217@gmail.com"
+password = "txcm ejgu vkei deqw"
+email_receiver = "anonymous77823@gmail.com"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        subject = request.form.get('subject')
+        body = request.form.get('message')
+        em = EmailMessage()
+        em["From"] = email_sender
+        em["To"] = email_receiver
+        em["Subject"] = subject
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+            smtp.login(email_sender, password)
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
+        return redirect(url_for('index'))
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
